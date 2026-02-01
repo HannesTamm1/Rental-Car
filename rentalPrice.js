d6e77a377971cd459a3ed181e111d70c1ac6d757
Hannes Tamm <hannes.tamm@tptlive.ee>
2026-02-01 14:11:19 +0200

    Correct namings, fix logic, make the code more readable

--- rentalPrice.js
+++ rentalPrice.js
@@ -1,8 +1,22 @@
 function price(pickupDate, dropoffDate, type, age, licenseYears) {
 
-  const vehicleClass = getClass(type);
-  const days = get_days(pickupDate, dropoffDate);
+  const vehicleClass = type || "Unknown";
+  const days = getDays(pickupDate, dropoffDate);
   const season = getSeason(pickupDate);
+
+const SEASON = {
+  HIGH: "High",
+  LOW: "Low",
+};
+
+const VEHICLE_CLASS = {
+  COMPACT: "Compact",
+  ELECTRIC: "Electric",
+  CABRIO: "Cabrio",
+  RACER: "Racer",
+  UNKNOWN: "Unknown",
+};
+
 
   if (age < 18) {
       return "Driver too young - cannot quote the price";
@@ -16,47 +30,35 @@ function price(pickupDate, dropoffDate, type, age, licenseYears) {
     return "Driver must have a license for at least 1 year";
   }
   
-  let rentalprice = age * days;
+  let rentalPrice = age * days;
 
-  if (licenseYears < 2 && licenseYears >= 1 ) {
-    rentalprice *= 1.3;
+  const hasShortLicense = licenseYears >= 1 && licenseYears < 2;
+
+  if (hasShortLicense) {
+    rentalPrice *= 1.3;
   }
 
-  if (licenseYears < 3 && licenseYears >= 1 && season === "High") {
-    rentalprice *= 1.3 + 15;
+  if (licenseYears < 3 && licenseYears >= 1 && season === SEASON.HIGH) {
+    rentalPrice *= 1.3 + 15;
   }
 
 
-  if (type === "Racer" && age <= 25 && season === "High") {
-      rentalprice *= 1.5;
+  if (type === VEHICLE_CLASS.RACER && age <= 25 && season === SEASON.HIGH) {
+      rentalPrice *= 1.5;
   }
 
-  if (season === "High" ) {
-    rentalprice *= 1.15;
+  if (season === SEASON.HIGH ) {
+    rentalPrice *= 1.15;
   }
 
-  if (days > 10 && season === "Low" ) {
-      rentalprice *= 0.9;
+  if (days > 10 && season === SEASON.LOW) {
+      rentalPrice *= 0.9;
   }
-  return '$' + rentalprice;
+  return '$' + rentalPrice;
 }
 
-function getClass(type) {
-  switch (type) {
-      case "Compact":
-          return "Compact";
-      case "Electric":
-          return "Electric";
-      case "Cabrio":
-          return "Cabrio";
-      case "Racer":
-          return "Racer";
-      default:
-          return "Unknown";
-  }
-}
 
-function get_days(pickupDate, dropoffDate) {
+function getDays(pickupDate, dropoffDate) {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   const firstDate = new Date(pickupDate);
   const secondDate = new Date(dropoffDate);
